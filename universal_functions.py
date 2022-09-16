c5ca145aa97db3bc9adfc20fb2ca4e885ba3e12c
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2022-09-16 14:09:19 +0100

    -

--- universal_functions.py
+++ universal_functions.py
@@ -14,11 +14,11 @@ from keys import *
 
 def report_analytic(network, wallet_address, display_name):
     try:
-        from keys import kusama_test_addresss
+        from keys import kusama_test_addresss, polkadot_test_addresss
         clean_message = f'Site hit from: {display_name}\n' \
                         f'Calling: {network}\n' \
                         f'https://wallety.org/kusama?wallet_address={wallet_address}'
-        if wallet_address not in kusama_test_addresss:
+        if wallet_address not in kusama_test_addresss or polkadot_test_addresss:
             requests.get(f'https://api.telegram.org/bot{telegram_api_key}/sendMessage?chat_id={telegram_chat_id_report_clean}&text={clean_message}')
         return None
     except:
